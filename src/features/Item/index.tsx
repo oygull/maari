47e175ac569c0709e753d1e8298946b1e044fd5e
oygull <oygulkhom3008@gmail.com>
2024-10-21 01:09:07 +0900

    home page changes

--- src/features/Item/index.tsx
+++ src/features/Item/index.tsx
@@ -2,20 +2,49 @@ import React, { useState } from 'react';
 import styles from "./item.module.scss"
 import productsArr from "../../app/assets/date/productsArr";
 import EachItem from "../EachItem";
+import Pagination from "@mui/material/Pagination";
+import { Box } from "@mui/material";
+
+const itemsPerPage = 12;
 
 function Item(){
-  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(productsArr.length / itemsPerPage);
+  const currentItems = productsArr.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+  const handlePageChange = (event: React.ChangeEvent<unknown>, page: number) => {
+    setCurrentPage(page);
+  };
 
   return (
     <div className="container">
       <h2 className={styles.headline}>Наши продукты</h2>
-          <div className={styles.wrapper}>
-      {
-        productsArr.map((item)=>
-          <EachItem item={item}/>
-        )
-      }
-    </div>
+      <div className={styles.wrapper}>
+        {currentItems.map((item) => (
+          <EachItem key={item.id} item={item} />
+        ))}
+      </div>
+      
+      <Box display="flex" justifyContent="center" mt={4}>
+        <Pagination
+          count={totalPages}
+          page={currentPage}
+          onChange={handlePageChange}
+          sx={{
+            "& .Mui-selected": {
+              backgroundColor: "pink",
+              color: "white", 
+            },
+            "& .MuiPaginationItem-root": {
+              color: "black", 
+            },
+          }}
+          shape="rounded"
+        />
+      </Box>
     </div>
   
   )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,87 @@ import React from "react";
 import styles from "./home.module.scss"
 import Hero from "../../features/Hero";
 import Brends from "../../features/Brends";
-import Item from "../../features/Item";
 import banner from "./banner.png"
+import productsArr from "../../app/assets/date/productsArr";
+import EachItem from "../../features/EachItem";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import banner2 from "./banner2.png"
+
+interface ArrowProps {
+  className?: string;
+  onClick?: () => void;
+}
+
+
+const NextArrow: React.FC<ArrowProps> = ({ className, onClick }) => {
+  return (
+    <div
+      className={'nextArrowHome'}
+      onClick={onClick}
+    >
+      <ArrowForwardIosIcon/>
+    </div>
+  );
+};
+
+const PrevArrow: React.FC<ArrowProps> = ({ className, onClick }) => {
+  return (
+    <div
+      className={'prevArrowHome'}
+      onClick={onClick}
+    >
+      <ArrowBackIosIcon/>
+    </div>
+  );
+};
 
 function HomePage(){
+
+  const filteredProducts = productsArr.filter(product => product.productReviews > 5);
+  const lastTenProducts = productsArr.slice(-10);
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    centerPadding: "30px",
+    centerMode: true,
+    nextArrow: <NextArrow />,  
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          centerPadding: "0px",
+       
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          centerPadding: "0",
+        },
+      },
+    ],
+  };
+
+
   return (
     <div className={styles.wrapper}>
         <Hero/>
@@ -13,8 +90,37 @@ function HomePage(){
         <div className={styles.innerWrap}>
           <img alt="banner" src={banner}/>
         </div>
-        <Item/>
-        
+        <div className="container">
+          <h2 className={styles.headline}>Самые популярные продукты</h2>
+          <div className={styles.sliderWrapper}>
+              <Slider {...settings}>
+              {filteredProducts.length > 0 ? (
+                      filteredProducts.map((item) => (
+                        <EachItem key={item.id} item={item} />
+                      ))
+                    ) : (
+                      <p>No products found with more than 5 reviews.</p>
+                    )}
+              </Slider>
+           
+          </div>
+        </div>
+        <div className={styles.innerWrap2}>
+          <img alt="banner2" src={banner2}/>
+        </div>
+        <div className="container">
+          <h2 className={styles.headline}>Новинки</h2>
+          <div className={styles.sliderWrapper}>
+              <Slider {...settings}>
+              {
+                      lastTenProducts.map((item) => (
+                        <EachItem key={item.id} item={item} />
+                      ))
+                   }
+              </Slider>
+           
+          </div>
+        </div>
     </div>
   )
 } 
